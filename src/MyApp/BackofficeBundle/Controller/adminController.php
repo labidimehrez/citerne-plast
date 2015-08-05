caa24def3efb6b4544a2995096f8865139903c79
MehezLabidi <mehrez.labidi@esprit.tn>
2015-08-05 11:44:40 +0000

    pages back end Signed-off-by: MehezLabidi <mehrez.labidi@esprit.tn>

--- src/MyApp/BackofficeBundle/Controller/adminController.php
+++ src/MyApp/BackofficeBundle/Controller/adminController.php
@@ -10,4 +10,49 @@ class adminController extends Controller
     {
         return $this->render('MyAppBackofficeBundle:admin:index.html.twig');
     }
+    
+     public function formAction()
+    {
+        return $this->render('MyAppBackofficeBundle:admin:form.html.twig');
+    }
+    
+     public function chartAction()
+    {
+        return $this->render('MyAppBackofficeBundle:admin:chart.html.twig');
+    }
+    
+     public function loginAction()
+    {
+        return $this->render('MyAppBackofficeBundle:admin:login.html.twig');
+    }
+    
+    public function registerAction()
+    {
+        return $this->render('MyAppBackofficeBundle:admin:register.html.twig');
+    }
+     
+    
+     public function tableAction()
+    {
+        return $this->render('MyAppBackofficeBundle:admin:table.html.twig');
+    }
+    
+    
+     public function uiAction()
+    {
+        return $this->render('MyAppBackofficeBundle:admin:ui.html.twig');
+    }
+    
+     public function tabpanelAction()
+    {
+        return $this->render('MyAppBackofficeBundle:admin:tabpanel.html.twig');
+    }
+    
+    
+    
+    
+    
+    
+    
+    
 }
