af8aad3951c7da2b2f7945f0f22656991ba94cad
MehezLabidi <mehrez.labidi@esprit.tn>
2015-08-05 14:24:23 +0000

    linkers backend Signed-off-by: MehezLabidi <mehrez.labidi@esprit.tn>

--- src/MyApp/BackofficeBundle/Controller/adminController.php
+++ src/MyApp/BackofficeBundle/Controller/adminController.php
@@ -2,57 +2,60 @@
 
 namespace MyApp\BackofficeBundle\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class adminController extends Controller
-{
-    public function indexAction()
-    {
+class adminController extends Controller {
+
+    public function indexAction(Request $request) {
         return $this->render('MyAppBackofficeBundle:admin:index.html.twig');
     }
-    
-     public function formAction()
-    {
+
+    public function formAction(Request $request) {
         return $this->render('MyAppBackofficeBundle:admin:form.html.twig');
     }
-    
-     public function chartAction()
-    {
+
+    public function chartAction(Request $request) {
         return $this->render('MyAppBackofficeBundle:admin:chart.html.twig');
     }
-    
-     public function loginAction()
-    {
-        return $this->render('MyAppBackofficeBundle:admin:login.html.twig');
+
+    public function loginAction(Request $request) {
+
+        $form = $this->createFormBuilder()
+                ->add('identifiant', 'text')
+                ->add('password', 'password')
+                ->getForm();
+        return $this->render('MyAppBackofficeBundle:admin:login.html.twig', array(
+                    'form' => $form->createView(),
+        ));
     }
-    
-    public function registerAction()
-    {
-        return $this->render('MyAppBackofficeBundle:admin:register.html.twig');
+
+    public function logoutAction(Request $request) {
+        //    détruire la session ici
+        return $this->redirect($this->generateUrl('my_app_backoffice_login'));
     }
-     
-    
-     public function tableAction()
-    {
+
+    public function registerAction(Request $request) {
+
+        $form = $this->createFormBuilder()
+                ->add('identifiant', 'text')
+                ->add('password', 'password')
+                ->getForm();
+        return $this->render('MyAppBackofficeBundle:admin:register.html.twig', array(
+                    'form' => $form->createView(),
+        ));
+    }
+
+    public function tableAction(Request $request) {
         return $this->render('MyAppBackofficeBundle:admin:table.html.twig');
     }
-    
-    
-     public function uiAction()
-    {
+
+    public function uiAction(Request $request) {
         return $this->render('MyAppBackofficeBundle:admin:ui.html.twig');
     }
-    
-     public function tabpanelAction()
-    {
+
+    public function tabpanelAction(Request $request) {
         return $this->render('MyAppBackofficeBundle:admin:tabpanel.html.twig');
     }
-    
-    
-    
-    
-    
-    
-    
-    
+
 }
